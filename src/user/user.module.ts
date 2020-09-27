e346b73c73a95cf69edea3fba67f269b16b4a56d
Ivan <sluvkann@polytechnic.poltava.ua>
2020-09-27 21:16:19 +0300

    Change database credentials

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,9 +6,10 @@ import UserService from './user.service';
 import ScoreService from './score.service';
 import Score from '../entity/Score';
 import FriendshipService from './friendship.service';
+import Friendship from '../entity/Friendship';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Score])],
+  imports: [TypeOrmModule.forFeature([User, Score, Friendship])],
   providers: [UserService, ScoreService, FriendshipService],
   controllers: [UserController],
   exports: [UserService, ScoreService, FriendshipService],
